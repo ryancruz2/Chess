2c2427a0810717da7140e4dd5750e6aeb4be264f
Ryan Eduardo Leandro da Cruz <rcruz@bdsdatasolution.com.br>
2023-06-02 16:57:01 -0300

    colocando cores nas peças

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -3,16 +3,21 @@ using Chess.Domain.Exceptions;
 
 try
 {
-    //Tabuleiro tab = new(8, 8);
+    Tabuleiro tab = new(8, 8);
 
-    //tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
-    //tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 7));
-    //tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
-    //Tela.ImprimirTabuleiro(tab);
+    tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
+    tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 7));
+    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
 
-    PosicaoXadrez pos = new('c', 7);
-    Console.WriteLine(pos);
-    Console.WriteLine(pos.ToPosicao());
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(7, 7));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(7, 0));
+    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(7, 3));
+
+    Tela.ImprimirTabuleiro(tab);
+
+    //PosicaoXadrez pos = new('c', 7);
+    //Console.WriteLine(pos);
+    //Console.WriteLine(pos.ToPosicao());
 
 } catch(TabuleiroException e)
 {
--- Chess/View/Tela.cs
+++ Chess/View/Tela.cs
@@ -3,15 +3,41 @@ class Tela
 {
     public static void ImprimirTabuleiro(Tabuleiro tab)
     {
-        for (int i = 0; i < tab.linhas; i++) {
-            for(int j = 0; j < tab.colunas; j++) {
+        for (int i = 0; i < tab.linhas; i++)
+        {
+            Console.Write($"{8 - i} ");
+            for (int j = 0; j < tab.colunas; j++)
+            {
                 if (tab.peca(i, j) is null)
                     Console.Write("- ");
                 else
-                    Console.Write($"{tab.peca(i, j)} ");
+                    ImprimirPeca(tab.peca(i, j));
+
             }
             Console.WriteLine();
         }
+        Console.WriteLine("  a b c d e f g h ");
     }
 
+    public static void ImprimirPeca(Peca peca)
+    {
+        switch (peca.cor)
+        {
+            case Cor.Branca:
+                Console.Write($"{peca} ");
+                break;
+            case Cor.Preta:
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{peca} ");
+                    Console.ForegroundColor = aux;
+                    break;
+                }
+            default:
+                Console.Write($"{peca} ");
+                break;
+        }
+
+    }
 }
